9036067b9fabab1fba2a447b14ef176a08c71564
abc56pp2ab <p19596@163.com>
2019-10-10 16:34:42 +0800

    Add LeetCode

---
+++ Languages/C++/123.cpp
@@ -0,0 +1,3 @@
+/*------ Description ------
+Author: Paddy Huang
+*/
--- manager.sh
+++ manager.sh
@@ -7,6 +7,7 @@ Menu(){
 	echo "######--Menu--######"
 	echo "# 1. Browse Direstories."
 	echo "# 2. Git add & commit & push."
+	echo "# 3. New file."
 	echo "# 0. Exit"
 	echo "######--Menu--######"
 }
@@ -35,6 +36,22 @@ GitControl(){
 	git add .; git commit -m "$info"; git push
 }
 
+# 3. New files.
+NewFiles(){
+	echo "# 3. New files:"
+	echo "1. C++"
+	read -p "Choose: " types
+	case $types in
+		1)	read -p "Name: " name
+			cd $root/Languages/C\+\+/; touch $name.cpp
+			createdDay < date
+			echo createdDay
+			echo "/*------ Description ------\nAuthor: Paddy Huang\n*/" > $name.cpp	;;
+		*)	;;
+	esac
+
+}
+
 # 0. Exit
 Exit(){
 	echo "# 0. Exit"
@@ -49,6 +66,7 @@ do
 	case $option in
 		1)	BrowseDirectories;;
 		2)	GitControl;;
+		3)	NewFiles;;
 		0)	Exit;;
 		*)	clear; echo "No Options";;
 	esac
