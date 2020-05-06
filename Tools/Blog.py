607c94d8b4d8082dc62d3b2adc635d762de88075
PaddyHuang <huangblcs@foxmail.com>
2020-05-06 15:43:47 +0800

    Remove files from branch raspbian

--- Tools/Blog.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python3
-import os
-
-root = '/Users/Polin/Documents/blog'
-os.chdir(root)
-os.system('open -a "/Applications/Utilities/Terminal.app" ' + root)
-os.system('code ' + root)
-
