344de1438042d3acd4bc3f4fa3cbc35289e48601
PaddyH <huangblcs@foxmail.com>
2020-05-30 14:26:38 +0800

    Update install.sh

--- Mac/install.sh
+++ Mac/install.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Alter hosts
-sudo cp /etc/hosts /etc/hosts.backup
-sudo mv ./hosts /etc/hosts
-
 # Install brew
-sudo ruby ./brew_install.rb
-
-# Use tsinghua mirror for brew
-git -C "$(brew --repo)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git
-git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git
-git -C "$(brew --repo homebrew/cask)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask.git
-git -C "$(brew --repo homebrew/cask-fonts)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-fonts.git
-git -C "$(brew --repo homebrew/cask-drivers)" remote set-url origin https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-cask-drivers.git
+/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
 
 brew update
 brew upgrade
 
-brew cask install iina hanshaker google-chrome wechat qq wps-office appcleaner docker
+brew cask install iina handshaker google-chrome wechat qq wpsoffice appcleaner docker ezip
