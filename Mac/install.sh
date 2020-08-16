b304a8b7a24e216486293182fe86325e6a7746ff
PaddyHuang <huangblcs@foxmail.com>
2020-08-16 10:27:50 +0800

    Remove Tools/Alarm & Mac/

--- Mac/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install brew
-/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
-
-brew update
-brew upgrade
-
-brew cask install iina handshaker google-chrome wechat qq wpsoffice appcleaner docker ezip
